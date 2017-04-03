ee3e0c995f0bf50d1ee36afc9d1c2a722515fc07
Jam Risser <jamrizzi@users.noreply.github.com>
2017-04-03 13:16:07 -0500

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,6 +1,6 @@
 import numpy as np
-def neural_net(X=np.array([[1, 0, 0], [0, 0, 1], [1, 0, 0], [0, 0, 1]]), y=np.array([[1, 0, 1, 0]]).T, syn0=(2 * np.random.random((len(X[0]),1)) - 1)):
+def neural_net(X=np.array([[1,0,0],[0,0,1],[1,0,0],[0,0,1]]),y=np.array([[1,0,1,0]]).T,syn0=(2*np.random.random((len(X[0]),1))-1)):
     for i in xrange(100000):
-        l1 = 1 / (1 + np.exp(-(np.dot(X, syn0))))
-        syn0 = np.dot(X.T, y - l1)
-    return {'syn0': syn0, 'l1': l1}
+        l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
+        syn0 = np.dot(X.T,y-l1)
+    return {'syn0':syn0,'l1':l1}
